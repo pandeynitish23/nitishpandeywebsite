7cf4270a9a9053572b565383cb6ff49862b6056c
Nitish Pandey <90491667+pandeynitish23@users.noreply.github.com>
2026-03-01 14:54:45 +0530

    Update LinkedIn profile link in script.js

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ function initContactForm() {
 
   // My contact info
   const WHATSAPP_NUMBER = '919234770717';
-  const LINKEDIN_PROFILE = 'https://www.linkedin.com/in/nitish-kumar-pandey-455b641b3/';
+  const LINKEDIN_PROFILE = 'https://www.linkedin.com/messaging/thread/2-ZDNhNjBjNDktNzVhYS00YTc2LTg5MDEtNzczMWEwNjdkNGFiXzEwMA==/';
   const MY_EMAIL = 'nitishkumar.nk.np@gmail.com';
 
   let activeChannel = 'whatsapp';
